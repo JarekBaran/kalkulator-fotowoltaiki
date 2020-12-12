4e33e653ab14e8d0c2fc149b5e25d05c2601b90a
Jarek Baran <git@jarekbaran.dev>
2020-12-12 23:06:32 +0100

    Added energy operators demo data

--- pv-calculator.js
+++ pv-calculator.js
@@ -1,7 +1,22 @@
 const pvCalculator = Vue.createApp({
   data() {
     return {
-        test: 'działa',
+      pvBill: {
+        monthlyBill: 300,
+        energyPrice: 0,
+        operators: [
+          { name: 'PGE', energyPrice: '0.64' },
+          { name: 'Enea', energyPrice: '0.59' },
+          { name: 'Energa', energyPrice: '0.67' },
+          { name: 'Tauron', energyPrice: '0.60' },
+          { name: 'Innogy', energyPrice: '0.58' },
+        ]
+      }
+    }
+  },
+  methods: {
+    calculation() {
+      console.log('CALCULATION');
     }
   }
 });
