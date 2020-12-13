3e323df149051efd88ff37d3055e331786bc2111
Jarek Baran <git@jarekbaran.dev>
2020-12-13 15:31:45 +0100

    Added logic to pv-parameters card

--- pv-calculator.js
+++ pv-calculator.js
@@ -11,6 +11,42 @@ const pvCalculator = Vue.createApp({
           { name: 'Tauron', energyPrice: '0.60' },
           { name: 'Innogy', energyPrice: '0.58' },
         ]
+      },
+      pvParameters: {
+        installationTax: 8,
+        installationPlace: [
+          { place: 'budynek mieszkalny', tax: 8 },
+          { place: 'garaż poza domem', tax: 23 },
+          { place: 'inny budynek', tax: 23 },
+          { place: 'konstrukcja na gruncie', tax: 23 },
+        ],
+        mountedCost: 1,
+        installatioMounted: [
+          { on: 'dachówke ceramiczną', cost: 1.1 },
+          { on: 'blachodachówkę', cost: 1 },
+          { on: 'płytę betonową', cost: 0.8 },
+          { on: 'gont dachowy', cost: 0.9 },
+          { on: 'inny materiał', cost: 1.2 },
+        ],
+        orientationSunProfit: 1,
+        installationOrientation: [
+          { direction: 'Wschód (E)', sunProfit: 0.7 },
+          { direction: 'Zachód (W)', sunProfit: 0.7 },
+          { direction: 'Południe (S)', sunProfit: 1 },
+          { direction: 'Południowy Wschód (SE)', sunProfit: 0.8 },
+          { direction: 'Południowy Zachód (SW)', sunProfit: 0.9 },
+          { direction: 'Północ (N)', sunProfit: 0.5 },
+          { direction: 'Północny Wschód (NE)', sunProfit: 0.6 },
+          { direction: 'Północny Zachód (NW)', sunProfit: 0.6 },
+        ],
+        installationPerformance: 1,
+        installationTilt: [
+          { percent: '15%', performance: 0.7 },
+          { percent: '30%', performance: 1 },
+          { percent: '45%', performance: 0.9 },
+          { percent: '60%', performance: 0.8 },
+          { percent: '75%', performance: 0.6 },
+        ],
       }
     }
   },
@@ -23,9 +59,6 @@ const pvCalculator = Vue.createApp({
     energyConsumption(months) {
       return Math.round((this.pvBill.monthlyBill / this.pvBill.energyPrice) * months);
     },
-    calculation() {
-      return console.log('CALCULATION');
-    }
   }
 });
 
